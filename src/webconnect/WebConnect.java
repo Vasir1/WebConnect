ac11acc4b1a282eb42e26c440bb42642861658cf
Vasir1 <fullmetilalchemist@gmail.com>
2014-08-04 00:49:59 -0400

    Bool array to check if people are sitting.

--- src/webconnect/WebConnect.java
+++ src/webconnect/WebConnect.java
@@ -11,46 +11,21 @@ import java.net.Socket;
  * and open the template in the editor.
  */
 
-
+////COMMENT
 /**
  *
  * @author Kyle
  */
 public class WebConnect {
 
-    private static ServerSocket serverSocket;
-    private static Socket clientSocket;
-    private static InputStreamReader inputStreamReader;
-    private static BufferedReader bufferedReader;
-    private static String message;
+    
     public WebConnect(){}
-    public static void main(String[] args) {
+    public static  void main(String[] args) {
+        Monitor monitor =new Monitor();
+        monitor.StartServer();
 
-        try {
-            serverSocket = new ServerSocket(7575);  //Server socket
-
-        } catch (IOException e) {
-            System.out.println("Could not listen on port: 7575");
-        }
-
-        System.out.println("Server started. Listening to the port 7575");
-
-        while (true) {
-            try {
-
-                clientSocket = serverSocket.accept();   //accept the client connection
-                inputStreamReader = new InputStreamReader(clientSocket.getInputStream());
-                bufferedReader = new BufferedReader(inputStreamReader); //get the client message
-                message = bufferedReader.readLine();
-
-                System.out.println(message);
-                inputStreamReader.close();
-                clientSocket.close();
-
-            } catch (IOException ex) {
-                System.out.println("Problem in message reading");
-            }
-        }
+       
 
     }
+    
 }
